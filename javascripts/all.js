603051576947601138d0854d59160f260ede337c
Ying Tzu <betty27201983@gmail.com>
2022-01-22 18:39:51 +0800

    update1

---
+++ javascripts/all.js
@@ -0,0 +1,8 @@
+$("#twzipcode").twzipcode({
+    countySel: "縣市", // 城市預設值, 字串一定要用繁體的 "臺", 否則抓不到資料
+    districtSel: "鄉鎮市區", // 地區預設值
+    zipcodeIntoDistrict: true, // 郵遞區號自動顯示在地區
+    css: ["form-select city form-control", "form-select town form-control"], // 自訂 "城市"、"地區" class 名稱
+    countyName: "city", // 自訂城市 select 標籤的 name 值
+    districtName: "town" // 自訂地區 select 標籤的 name 值
+});
